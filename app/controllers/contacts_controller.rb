f583c1ba444e46594018973b5bf8120eb133ee5a
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-04-30 19:19:30 -0400

    Add Users factory. Implement more contacts controller tests

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < ApplicationController
     before_action :authenticate, except: [:index, :show]
     before_action :set_contact, only: [:show, :edit, :update, :destroy]
-    
+
     # GET /contacts
     # GET /contacts.json
     def index
