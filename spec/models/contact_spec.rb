4ca8b454649c475eacb463f08a7f030f61dad1c5
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-05-02 21:47:56 -0400

    Further refactor contact spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -19,28 +19,26 @@ describe Contact do
     it { should validate_uniqueness_of  :email }
 
     it "returns a contact's full name as a string" do
-        contact = build(:contact,
+        contact = build_stubbed(:contact,
             firstname: "Jane", lastname: "Doe"
         )
         expect(contact.name).to eq "Jane Doe"
     end
 
     describe "filter last name by letter" do
-        before :each do
-            @smith      = create(:contact, lastname: "Smith")
-            @jones      = create(:contact, lastname: "Jones")
-            @johnson    = create(:contact, lastname: "Johnson")
-        end
+        let(:smith)     { create(:contact, lastname: 'Smith') }
+        let(:jones)     { create(:contact, lastname: 'Jones') }
+        let(:johnson)   { create(:contact, lastname: 'Johnson') }
 
         context "matching letters" do
             it 'returns a sorted array of results that match' do
-                expect(Contact.by_letter('J')).to eq [@johnson, @jones]
+                expect(Contact.by_letter('J')).to eq [johnson, jones]
             end
         end
 
         context "non-matching letters" do
             it 'omits results that do not match' do
-                expect(Contact.by_letter('J')).not_to include @smith
+                expect(Contact.by_letter('J')).not_to include smith
             end
         end
     end
